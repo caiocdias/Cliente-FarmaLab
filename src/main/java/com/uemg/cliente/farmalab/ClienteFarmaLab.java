da37ef2e7211a19ce5f96ef995e846a764fc4dda
Caio Cezar Dias <caiocd007@gmail.com>
2025-01-15 22:27:09 -0300

    Initial Commit

---
+++ src/main/java/com/uemg/cliente/farmalab/ClienteFarmaLab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uemg.cliente.farmalab;
+
+/**
+ *
+ * @author Caio Cezar Dias
+ */
+public class ClienteFarmaLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
